99fb6791b46eccfb5c89f9c30651d6a4bcbd68d4
Sarrruman <sjovac@gmail.com>
2016-02-21 17:16:53 +0100

    Finish log in/log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,10 @@ class SessionsController < ApplicationController
       user = User.find_by(email: params[:session][:email].downcase)
       if user && user.authenticate(params[:session][:password])
         # Log the user in and redirect to the user's show page.
-        login(user)
+        log_in(user)
+        #remember user # pomocu cooky-ja
+        #pita da li je checkbox==1 i odlucuje da li da sacuva korisnika
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
         redirect_to user
       else
         flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
@@ -16,7 +19,7 @@ class SessionsController < ApplicationController
     end
 
     def destroy
-      log_out
+      log_out if logged_in?
       redirect_to root_url
     end
 
